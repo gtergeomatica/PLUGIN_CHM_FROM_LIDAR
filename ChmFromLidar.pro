90395850856c7961fc4599d8d104680251e4f33d
RobiFag <robifagandini@hotmail.it>
2019-06-18 18:09:37 +0200

    add translate files

---
+++ ChmFromLidar.pro
@@ -0,0 +1,7 @@
+SOURCES = __init__.py \
+          chm_from_lidar_dialog.py \
+          chm_from_lidar.py
+
+FORMS = chm_from_lidar_dialog_base.ui
+
+TRANSLATIONS = i18n/ChmFromLidar_it.ts
---
+++ i18n/ChmFromLidar_it.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ChmFromLidarDialogBase</name>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="23"/>
+        <source>CHM from LIDAR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="33"/>
+        <source>Select an AOI</source>
+        <translation>Seleziona area di intresse</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="83"/>
+        <source>Using selected features</source>
+        <translation>Usa solo le geometrie selezionate</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="95"/>
+        <source>Select the autority</source>
+        <translation>Seleziona Ente</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="144"/>
+        <source>Select a campaign</source>
+        <translation>Seleziona campagna</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="196"/>
+        <source>Clip file name</source>
+        <translation>Nome del file Clip</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="210"/>
+        <source>Choose resolution (m)</source>
+        <translation>Scegli la risoluzione (m)</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="231"/>
+        <source>Onput CHM folder</source>
+        <translation>Cartella di output</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="260"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="287"/>
+        <source>CRS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="316"/>
+        <source>remove values &lt; 0</source>
+        <translation>rimuovi valori &lt; 0</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="342"/>
+        <source>remove values &gt;</source>
+        <translation>rimuovi valori &gt;</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="363"/>
+        <source>Clear Log</source>
+        <translation>Pulisci il Log</translation>
+    </message>
+    <message>
+        <location filename="../chm_from_lidar_dialog_base.ui" line="383"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
